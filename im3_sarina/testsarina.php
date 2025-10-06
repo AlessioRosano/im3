5642678bc3ce856e1086fd37cacfeec3a2652162
sarinadaros <sarina.daros@stud.fhgr.ch>
2025-10-06 14:28:16 +0200

    Anpassungen Vormittag

---
+++ im3_sarina/testsarina.php
@@ -0,0 +1,40 @@
+<?php
+
+<<<<<<< HEAD
+$name = "Sarina";
+echo "Hallo $name, wie gehtrtt's?";
+=======
+$name = "Sarina Da";
+echo "Hallo $name, wie geht's?";
+>>>>>>> aee26099738ddbc1eff96cbd082cf494349dca90
+
+
+echo '<br>';
+
+$a = 10;
+$b = 10;
+echo $a + $b;
+
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> aee26099738ddbc1eff96cbd082cf494349dca90
+
+
+// echo multiply(5, 10); //-->anstatt console.log in JS
+
+
+$banane = array("gelb", "süss", 5); 
+
+//--> zum testen wichtig!
+echo '<pre>';
+print_r($banane);
+echo '</pre>';
+<<<<<<< HEAD
+
+
+?>
+=======
+ 
+>>>>>>> aee26099738ddbc1eff96cbd082cf494349dca90
